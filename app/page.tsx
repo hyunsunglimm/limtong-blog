c329ad5c756aaf491d87a01442fade140cb36726
hyunsunglimm <gustjd3834@naver.com>
2024-09-24 00:04:03 +0900

    style: 아이폰 폰트 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function page() {
   return (
     <div>
-      <p className="text-4xl font-bold">asd</p>
+      <p className="text-4xl font-bold">asd, ;</p>
+      <p>asd, ;</p>
     </div>
   );
 }
