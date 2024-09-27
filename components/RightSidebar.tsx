124a9214d5c0c833717d3073acc4b46d45221e8d
hyunsunglimm <gustjd3834@naver.com>
2024-09-27 16:49:27 +0900

    feat: Toc

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -4,6 +4,7 @@ import useWidth from "@/hooks/useWidth";
 import { useState } from "react";
 import RightMenuIcon from "./icons/RightMenuIcon";
 import useOutside from "@/hooks/useOutside";
+import Toc from "./Toc";
 
 const END_POINT = 1024;
 
@@ -30,22 +31,9 @@ export default function RightSidebar() {
       >
         <RightMenuIcon />
       </button>
-      <ul className="grow overflow-y-scroll pb-12">
-        <li className="break-words">Google Analytics란?</li>
-        <li className="break-words">사용자 행동 분석</li>
-        <li className="break-words">유입 경로 분석</li>
-        <li className="break-words">목표 및 전환 추적</li>
-        <li className="break-words">고급 보고서</li>
-        <li className="break-words">Google Analytics를 사용하는 이유</li>
-        <li className="break-words">1. 사용자 행동 분석</li>
-        <li className="break-words">2. 마케팅 캠페인 성과 측정</li>
-        <li className="break-words">3. 웹사이트 성능 개선</li>
-        <li className="break-words">4. 타겟팅 및 개인화</li>
-        <li className="break-words">5. 데이터 기반 의사결정</li>
-        <li className="break-words">Google Analytics 적용하기</li>
-        <li className="break-words">Google Analytics 설정</li>
-        <li className="break-words">프로젝트에 적용</li>
-      </ul>
+      <div className="grow overflow-y-scroll pb-12">
+        <Toc />
+      </div>
     </div>
   );
 }
---
+++ components/Toc.tsx
@@ -0,0 +1,57 @@
+import { getIntersectionObserver } from "@/utils/observer";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type TocItem = {
+  id: string;
+  tagName: string;
+  title: string;
+};
+
+export default function Toc() {
+  const [currentId, setCurrentId] = useState<string>("");
+  const [tocList, setTocList] = useState<TocItem[]>([]);
+
+  useEffect(() => {
+    const observer = getIntersectionObserver(setCurrentId);
+    const headingElements = Array.from(document.querySelectorAll("h2, h3"));
+
+    const headingData = headingElements.map((header) => ({
+      id: header.id,
+      tagName: header.tagName.toLowerCase(),
+      title:
+        (header.children[0] as HTMLElement).innerText.replace("#", "") ||
+        "타이틀이 없습니다.",
+    }));
+
+    setTocList(headingData);
+
+    headingElements.map((header) => {
+      observer.observe(header);
+    });
+  }, []);
+
+  return (
+    <section>
+      {tocList.length === 0 && (
+        <p className="text-center text-neutral-400">
+          해당 글은 목차가 없습니다.
+        </p>
+      )}
+      <ul className="flex flex-col gap-2">
+        {tocList.map(({ id, tagName, title }) => (
+          <li key={id} className={`${tagName === "h3" && "ml-4 text-[15px]"}`}>
+            <Link
+              href={`#${id}`}
+              className={`${
+                currentId === id && "text-my"
+              } hover:text-my transition`}
+            >
+              {title}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
