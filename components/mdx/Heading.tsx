9af24fca0ac3465f235443522ec59fcee3ef35a5
hyunsunglimm <gustjd3834@naver.com>
2024-09-30 11:34:05 +0900

    fix: heading태그 해시링크 제거

--- components/mdx/Heading.tsx
+++ components/mdx/Heading.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { ReactNode } from "react";
 
 type HeadingProps = {
@@ -8,66 +7,36 @@ type HeadingProps = {
 
 export const h1 = ({ id, children }: HeadingProps) => (
   <h1 id={id} className="text-4xl font-bold mt-12 mb-4 group">
-    <Link href={`#${id}`}>
-      {children}
-      <span className="ml-4 opacity-0 group-hover:opacity-100 transition">
-        #
-      </span>
-    </Link>
+    {children}
   </h1>
 );
 
 export const h2 = ({ id, children }: HeadingProps) => (
   <h2 id={id} className="text-3xl font-bold mt-10 mb-4 group">
-    <Link href={`#${id}`}>
-      {children}
-      <span className="ml-4 opacity-0 group-hover:opacity-100 transition">
-        #
-      </span>
-    </Link>
+    {children}
   </h2>
 );
 
 export const h3 = ({ id, children }: HeadingProps) => (
   <h3 id={id} className="text-2xl font-bold mt-8 mb-4 group">
-    <Link href={`#${id}`}>
-      {children}
-      <span className="ml-4 opacity-0 group-hover:opacity-100 transition">
-        #
-      </span>
-    </Link>
+    {children}
   </h3>
 );
 
 export const h4 = ({ id, children }: HeadingProps) => (
   <h4 id={id} className="text-xl font-bold mt-6 mb-4 group">
-    <Link href={`#${id}`}>
-      {children}
-      <span className="ml-4 opacity-0 group-hover:opacity-100 transition">
-        #
-      </span>
-    </Link>
+    {children}
   </h4>
 );
 
 export const h5 = ({ id, children }: HeadingProps) => (
   <h5 id={id} className="text-lg font-bold mt-4 mb-2 group">
-    <Link href={`#${id}`}>
-      {children}
-      <span className="ml-4 opacity-0 group-hover:opacity-100 transition">
-        #
-      </span>
-    </Link>
+    {children}
   </h5>
 );
 
 export const h6 = ({ id, children }: HeadingProps) => (
   <h6 id={id} className="font-bold mt-4 mb-2 group">
-    <Link href={`#${id}`}>
-      {children}
-      <span className="ml-4 opacity-0 group-hover:opacity-100 transition">
-        #
-      </span>
-    </Link>
+    {children}
   </h6>
 );
