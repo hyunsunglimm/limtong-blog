8d5a3de57229da8ca4ced054f7682a0b8729ebc2
hyunsunglimm <gustjd3834@naver.com>
2024-10-01 23:06:54 +0900

    test: post 페이지 메인 컨텐츠 & right sidebar 제거

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { getPost } from "@/service/post";
 import { Metadata } from "next";
 import LeftSidebar from "@/components/LeftSidebar";
-import RightSidebar from "@/components/RightSidebar";
 
 type PostDetailPageProps = {
   params: {
@@ -11,14 +10,14 @@ type PostDetailPageProps = {
 
 export default async function PostDetailPage({ params }: PostDetailPageProps) {
   const { slug } = params;
+  const post = await getPost(slug);
 
   return (
     <div className="flex">
       <LeftSidebar />
       <div className="md:pl-[60px] xl:pl-[300px] md:pr-[60px] lg:pr-[300px] limit:pr-[316px] w-full">
-        <p>{slug}</p>
+        <p>{post.title}</p>
       </div>
-      <RightSidebar />
     </div>
   );
 }
