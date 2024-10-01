72428130e7d66c7db10269df53ec18c2da982f34
hyunsunglimm <gustjd3834@naver.com>
2024-10-01 19:01:31 +0900

    test: 사이드바 되돌리기

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,8 +1,17 @@
 import { getPost } from "@/service/post";
+import dynamic from "next/dynamic";
 import PostBody from "./components/PostBody";
 import PostHeader from "./components/PostHeader";
 import Comments from "@/components/Comments";
 import { Metadata } from "next";
+
+const LeftSidebar = dynamic(() => import("@/components/LeftSidebar"), {
+  ssr: false,
+});
+
+const RightSidebar = dynamic(() => import("@/components/RightSidebar"), {
+  ssr: false,
+});
 
 type PostDetailPageProps = {
   params: {
@@ -16,11 +25,13 @@ export default async function PostDetailPage({ params }: PostDetailPageProps) {
 
   return (
     <div className="flex">
+      <LeftSidebar />
       <div className="md:pl-[60px] xl:pl-[300px] md:pr-[60px] lg:pr-[300px] limit:pr-[316px] w-full">
         <PostHeader post={post} />
         <PostBody content={post.content} />
         <Comments />
       </div>
+      <RightSidebar />
     </div>
   );
 }
