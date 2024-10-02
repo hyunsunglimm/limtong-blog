7d0c8259acb243ff282437c4bb83453128f1b4d8
hyunsunglimm <gustjd3834@naver.com>
2024-10-02 19:25:13 +0900

    fix: 사용하지 않는 타입 제거

--- model/post.ts
+++ model/post.ts
@@ -1,19 +1,3 @@
-export type PostMatter = {
-  title: string;
-  date: Date;
-  desc: string;
-  thumbnail: string;
-};
-
-export type Post = PostMatter & {
-  slug: string;
-  url: string;
-  category: string;
-  dateString: string;
-  content: string;
-  readingMinutes: number;
-};
-
 export type SimplePost = {
   id: string;
   title: string;
