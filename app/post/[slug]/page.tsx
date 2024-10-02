f75e2dec638929125ff41e40141ae8f16bb48f73
hyunsunglimm <gustjd3834@naver.com>
2024-10-02 14:46:15 +0900

    test: 배포된 웹 랜더링 속도 측정

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -1,10 +1,6 @@
 import { getPost } from "@/service/post";
-import PostBody from "./components/PostBody";
-import PostHeader from "./components/PostHeader";
-import Comments from "@/components/Comments";
 import { Metadata } from "next";
 import LeftSidebar from "@/components/LeftSidebar";
-import RightSidebar from "@/components/RightSidebar";
 
 type PostDetailPageProps = {
   params: {
@@ -14,17 +10,13 @@ type PostDetailPageProps = {
 
 export default async function PostDetailPage({ params }: PostDetailPageProps) {
   const { slug } = params;
-  const post = await getPost(slug);
 
   return (
     <div className="flex">
       <LeftSidebar />
       <div className="md:pl-[60px] xl:pl-[300px] md:pr-[60px] lg:pr-[300px] limit:pr-[316px] w-full">
-        <PostHeader post={post} />
-        <PostBody content={post.content} />
-        <Comments />
+        <p>{slug}</p>
       </div>
-      <RightSidebar />
     </div>
   );
 }
